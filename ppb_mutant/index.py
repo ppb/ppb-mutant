70b8909f18c8d815103bdddd19ae2042f883ba58
Jamie Bliss <astronouth7303@gmail.com>
2018-10-20 19:27:46 -0400

    Attempt to do destiny-style view movement, find framerate issues

--- ppb_mutant/index.py
+++ ppb_mutant/index.py
@@ -7,6 +7,7 @@ import os.path
 import math
 from ppb_mutant import MutantSprite, load_index
 import pathlib
+from time import perf_counter
 
 
 class Region(ppb.BaseSprite):
@@ -43,7 +44,17 @@ class IndexScene(ppb.BaseScene):
         super().__init__(*p, pixel_ratio=64, **kw)
 
         for s in self.get_options():
-            self.add(s)
+            self.add(s, tags=['emoji'])
+
+        self.xmin = min(s.position.x - 0.5 for s in self.get(tag='emoji'))
+        self.xmax = max(s.position.x + 0.5 for s in self.get(tag='emoji'))
+        self.ymin = min(s.position.y - 0.5  for s in self.get(tag='emoji'))
+        self.ymax = max(s.position.y + 0.5 for s in self.get(tag='emoji'))
+
+        self.main_camera.position = ppb.Vector(
+            (self.xmin + self.xmax) / 2,
+            (self.ymin + self.ymax) / 2,
+        )
 
     def _load_index(self):
         with open('mutant/index.txt', 'rt') as indexfile:
@@ -64,7 +75,7 @@ class IndexScene(ppb.BaseScene):
             name = alias or shortcode
             if name not in emojis:
                 emojis.append(name)
-        return emojis[:256]
+        return emojis
 
     def get_options(self):
         emojis = self.compile_emojis()
@@ -73,7 +84,61 @@ class IndexScene(ppb.BaseScene):
         for emoji, pos in zip(emojis, self.grid(rows, cols, scale=1.1)):
             yield EmojiSprite(emoji=emoji, pos=pos)
         print(f"Loaded {len(emojis)} emoji")
+
+    def on_update(self, event, signal):
+        cam = self.main_camera
+        # https://github.com/ppb/pursuedpybear/issues/135
+        cam.frame_width = cam.viewport_width / cam.pixel_ratio
+        cam.frame_height = cam.viewport_height / cam.pixel_ratio
+        cam.half_width = cam.frame_width / 2
+        cam.half_height = cam.frame_height / 2
         
+    def on_mouse_motion(self, mouse, signal):
+        return
+        print("mouse", mouse.position)
+        x, y = mouse.position
+        cam = self.main_camera
+
+        if not (cam.frame_width and cam.frame_height):
+            return
+
+        frame_left, frame_right, frame_top, frame_bottom = \
+            cam.frame_left, cam.frame_right, cam.frame_top, cam.frame_bottom
+
+        frame_width, frame_height = cam.frame_width, cam.frame_height
+
+        # if not (self.xmin <= x <= self.xmax and self.ymin <= y <= self.ymax):
+        #     return
+
+        print("cam.x", frame_left, frame_width, frame_right)
+        print("cam.y", frame_top, frame_height, frame_bottom)
+        xpercent = (x - frame_left) / frame_width
+        ypercent = (y - frame_top) / frame_height
+
+        print("x", self.xmin, "->", self.xmax, "\t", xpercent)
+        print("y", self.ymin, "->", self.ymax, "\t", ypercent)
+
+        if xpercent < 0: xpercent = 0
+        if xpercent > 1: xpercent = 1
+        if ypercent < 0: ypercent = 0
+        if ypercent > 1: ypercent = 1
+
+        dest = ppb.Vector(
+            (self.xmax - self.xmin) * xpercent + self.xmin,
+            (self.ymax - self.ymin) * ypercent + self.ymin,
+        )
+        print("->", dest)
+
+        cam.position = dest
+
+    last_frame = None
+    def on_pre_render(self, event, signal):
+        now = perf_counter()
+        if self.last_frame is not None:
+            dt = now - self.last_frame
+            print(dt)
+        self.last_frame = now
+
 
 if __name__ == '__main__':
     ppb.run(IndexScene,
