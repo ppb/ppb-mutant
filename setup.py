91c88b672575a4d6650cc35a52743be99a574c49
Jamie Bliss <astronouth7303@gmail.com>
2018-11-02 03:51:04 -0400

    Increment versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='ppb-mutant',
-    version='0.4.2',
+    version='0.5.0',
     packages=['ppb_mutant'],
     package_data={'ppb_mutant': ['_assets/*.png', '_assets/*.txt']},
     install_requires=[
