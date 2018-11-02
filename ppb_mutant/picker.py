47987bdbc666ef6a5ee107d0387bae62c6291a1c
Jamie Bliss <astronouth7303@gmail.com>
2018-11-02 04:14:52 -0400

    Add a stand-alone picker

---
+++ ppb_mutant/picker.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Show the morph/tone picker.
+"""
+import ppb
+from ppb_mutant import SelectScene
+
+
+class SelectDemoScene(SelectScene):
+    def do_update_morphtone(self):
+        print(self.morph, self.tone)
+
+
+if __name__ == '__main__':
+    ppb.run(SelectDemoScene,
+        window_title='Mutant Morph/Tone Picker',
+    )
